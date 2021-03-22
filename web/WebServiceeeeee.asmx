83c9c91f198a425008812eb35ff14ba142a54b18
Mustafa Mustafayev <khalilzadehnurlan@gmail.com>
2021-03-22 23:36:11 +0400

    suallar

---
+++ web/WebServiceeeeee.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebServiceeeeee.asmx.cs" Class="web.WebServiceeeeee" %>
---
+++ web/WebServiceeeeee.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace web
+{
+    /// <summary>
+    /// Summary description for WebServiceeeeee
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServiceeeeee : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int  topla(int deyer1,int deyer2)
+        {
+            int result = deyer1 + deyer2;
+            return result;
+        }
+    }
+}
